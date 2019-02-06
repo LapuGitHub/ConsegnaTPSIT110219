748cbafde82624f137a8c6d2f1fd395660fb983d
Samuele <47148569+bsamuele@users.noreply.github.com>
2019-02-06 18:13:59 +0100

    Aggiunto codice produttore

--- ConsegnaTPSIT/Produttore/pch.h
+++ ConsegnaTPSIT/Produttore/pch.h
@@ -1,17 +1,3 @@
-// ConsegnaTPSIT.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
-//
-
-#include "pch.h"
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n"; 
-}
-
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
 // Suggerimenti per iniziare: 
 //   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
 //   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
@@ -19,3 +5,10 @@ int main()
 //   4. Usare la finestra Elenco errori per visualizzare gli errori
 //   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
 //   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
+
+#ifndef PCH_H
+#define PCH_H
+
+// TODO: aggiungere qui le intestazioni da precompilare
+
+#endif //PCH_H
